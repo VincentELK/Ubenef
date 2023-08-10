76d7749a55fda3c2c1de417004004924205ca929
Gazerbim <100291331+Gazerbim@users.noreply.github.com>
2023-08-10 17:00:18 +0200

    Add files via upload
    Completion du formulaire d'inscription + envoi des données dans une base de données

---
+++ index1.php
@@ -0,0 +1,12 @@
+<h1>MA page web</h1>
+<?php
+echo "<strong>Hello</strong><br>";
+echo "slt<br>";
+$variable = 1;
+while($variable != 10)
+{
+    $variable += 1;
+    echo $variable;
+}
+?>
+<?= "ceci est un raccourci echo" ?>
