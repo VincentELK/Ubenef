b115f55a1c6c2c0756574107b42015df10067729
guillaume.grdi <guillaumegrandi2008@gmail.com>
2024-01-14 08:05:51 +0100

    ajout side bar

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-//--------------------- BENEF CALCULATOR-------
+//--------------------- BENEF CALCULATOR----------------------//
 
-let getBenefice = calcul_btn.addEventListener("click", (e) => {
-  let gazPrice = document.getElementById("gas_price").value;
-  let maxTank = 42;
-  let priceFullTank = gazPrice * maxTank;
-  let distanceMax = 900;
-  let priceByDistance = priceFullTank / distanceMax;
+// let getBenefice = calcul_btn.addEventListener("click", (e) => {
+//   let gazPrice = document.getElementById("gas_price").value;
+//   let maxTank = 42;
+//   let priceFullTank = gazPrice * maxTank;
+//   let distanceMax = 900;
+//   let priceByDistance = priceFullTank / distanceMax;
 
-  const taxesInput = document.getElementById("taxes").value;
-  const racePrice = document.getElementById("race_price").value;
-  const userTrip = document.getElementById("distance_traveled").value;
-  const bonus = document.getElementById("bonus").value;
-  const benefice = document.getElementById("benefice");
-  const taxesPercentage = taxesInput / 100;
-  const userTripCost = priceByDistance * userTrip;
+//   const taxesInput = document.getElementById("taxes").value;
+//   const racePrice = document.getElementById("race_price").value;
+//   const userTrip = document.getElementById("distance_traveled").value;
+//   const bonus = document.getElementById("bonus").value;
+//   const benefice = document.getElementById("benefice");
+//   const taxesPercentage = taxesInput / 100;
+//   const userTripCost = priceByDistance * userTrip;
 
-  let result =
-    racePrice * (1 - taxesPercentage) - userTripCost + parseInt(bonus);
+//   let result =
+//     racePrice * (1 - taxesPercentage) - userTripCost + parseInt(bonus);
 
-  document.getElementById("benefice").textContent = result.toFixed(2);
+//   document.getElementById("benefice").textContent = result.toFixed(2);
 
-  e.preventDefault();
+//   e.preventDefault();
+// });
+
+//--------------------------- nav
+
+const nav_btn = document.getElementById("nav-btn");
+
+nav_btn.addEventListener("click", () => {
+  console.log("test");
 });
