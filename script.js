d9eaa4ce269edb187be3012c2f95fe1159c8e4e1
guillaume.grdi <guillaumegrandi2008@gmail.com>
2024-01-20 17:24:27 +0100

    part sur des bonnes bases

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ const calcul_btn = document.getElementById("calcul_btn");
 
 // -------------Create account------
 
-// tools_li.addEventListener("click", () => {
-//   window.open("tools.html", "_self");
-// });
+tools_li.addEventListener("click", () => {
+  window.open("tools.html", "_self");
+});
 
-// home_li.addEventListener("click", (e) => {
-//   window.open("index.html", "_self");
-// });
+home_li.addEventListener("click", (e) => {
+  window.open("index.html", "_self");
+});
