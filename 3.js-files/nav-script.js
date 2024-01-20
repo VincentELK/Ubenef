6b135f6a2b8ed29381aae86e6eb1c0c3f15d2a10
guillaume.grdi <guillaumegrandi2008@gmail.com>
2024-01-20 19:37:28 +0100

    ajout de l'ouverture du 1.login-signup.html au click d'un des deux bouttons de la nav

--- 3.js-files/nav-script.js
+++ 3.js-files/nav-script.js
@@ -1,7 +1,7 @@
 //-------------------------------Variable----------------------//
 const home_li = document.getElementById("home_li");
 const tools_li = document.getElementById("tools_li");
-
+const log_li = document.querySelectorAll(".log-nav > .li-container");
 // ------------------------------Script------------------------//
 tools_li.addEventListener("click", () => {
   window.open("1.tools.html", "_self");
@@ -10,4 +10,9 @@ tools_li.addEventListener("click", () => {
 home_li.addEventListener("click", () => {
   window.open("1.home.html", "_self");
 });
-console.log(home_li);
+
+log_li.forEach(function (bouton) {
+  bouton.addEventListener("click", function () {
+    window.open("1.login-signup.html", "_self");
+  });
+});
